3765950cc05a3efec1ebf2c059faadda175e7f6c
Hazel <e0420648@u.nus.edu>
2022-07-22 12:51:28 +0800

    add Wallets.js and ExchangeRate.js

---
+++ frontend/src/components/ExchangeRate.js
@@ -0,0 +1,93 @@
+import React, {useState} from 'react';
+
+export default function ExchangeRate() {
+    let exchangeRate = [
+        {
+          "id": 1,
+          "base_currency": "SGD",
+          "exchange_currency": "CAD",
+          "rate": 0.9255
+        },
+        {
+          "id": 2,
+          "base_currency": "SGD",
+          "exchange_currency": "CNH",
+          "rate": 4.7868
+        },
+        {
+          "id": 3,
+          "base_currency": "SGD",
+          "exchange_currency": "EUR",
+          "rate": 0.7086
+        },
+        {
+          "id": 4,
+          "base_currency": "SGD",
+          "exchange_currency": "HKD",
+          "rate": 5.5830
+        },
+        {
+          "id": 5,
+          "base_currency": "SGD",
+          "exchange_currency": "JPY",
+          "rate": 97.5303
+        },
+        {
+          "id": 6,
+          "base_currency": "SGD",
+          "exchange_currency": "NZD",
+          "rate": 1.1612
+        },
+        {
+          "id": 7,
+          "base_currency": "SGD",
+          "exchange_currency": "NOK",
+          "rate": 7.2912
+        },
+        {
+          "id": 8,
+          "base_currency": "SGD",
+          "exchange_currency": "GBP",
+          "rate": 0.5974
+        },
+        {
+          "id": 9,
+          "base_currency": "SGD",
+          "exchange_currency": "SEK",
+          "rate": 7.5168
+        },
+        {
+          "id": 10,
+          "base_currency": "SGD",
+          "exchange_currency": "THB",
+          "rate": 25.7275
+        },
+        {
+          "id": 11,
+          "base_currency": "SGD",
+          "exchange_currency": "USD",
+          "rate": 0.7113
+        }
+    ]
+
+    const [currency, setCurrency] = React.useState(exchangeRate);
+
+    return (
+        <div>
+            <table>
+            <tr key={"header"}>
+                {Object.keys(currency[0]).map((key) => (
+                <th>{key}</th>
+                ))}
+            </tr>
+            {currency.map((item) => (
+            <tr key={item.id}>
+                {Object.values(item).map((val) => (
+                <td>{val}</td>
+                ))}
+            </tr>
+            ))}
+            </table>
+        </div>
+    )
+}
