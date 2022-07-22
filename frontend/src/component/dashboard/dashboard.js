84fa0c12dcb15028e98c8e2ce39b0ee0ccaa01a8
HOWUHHOW <penghowt@gmail.com>
2022-07-22 13:54:09 +0800

    update dashboard.js add router

---
+++ frontend/src/component/dashboard/dashboard.js
@@ -0,0 +1,42 @@
+import "./dashboard.css";
+
+
+export default function Dashboard() {
+    return(
+        <>
+        <body>
+
+<img src="https://www.nicepng.com/png/full/267-2670427_1-dbs-group-holdings-ltd-dbs-bank-logo.png"
+        alt="logo"></img>
+        
+        <h2>Penghow Tan - Graduate Associate</h2>
+        
+        <header>
+          <h2>Project Management</h2>
+        </header>
+        
+        <section>
+          <nav>
+            <ul>
+              <li><a href="#">Expense Entry</a></li>
+              <li><a href="#">Expense History</a></li>
+              <li><a href="#">Submitted Claim</a></li>
+              <li><a href="#">Approved Claim</a></li>
+              <li><a href="#">List of Project</a></li>
+              <li><a href="#">Logout</a></li>
+              
+              
+            </ul>
+          </nav>
+          
+          <article>
+            <h1>London</h1>
+            <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+            <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+          </article>
+        </section>
+      
+        
+        </body></>
+    )
+}
