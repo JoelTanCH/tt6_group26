eacffa1ddaa9c9b00df093d80813dd8b53fe8ccb
Hazel <e0420648@u.nus.edu>
2022-07-22 12:27:45 +0800

    Dropdown for wallets

--- frontend/src/components/DashBoard.js
+++ frontend/src/components/DashBoard.js
@@ -1,7 +1,46 @@
+import React, {useState} from 'react';
 
 export default function DashBoard() {
+    let wallets = [
+        {
+          "id": 1,
+          "user_id": 1,
+          "name": "Multi-Currency Account"
+        },
+        {
+          "id": 2,
+          "user_id": 1,
+          "name": "Travel Account"
+        },
+        {
+          "id": 3,
+          "user_id": 2,
+          "name": "Trading Account"
+        },
+        {
+          "id": 4,
+          "user_id": 3,
+          "name": "Multi-Currency Account"
+        },
+        {
+          "id": 5,
+          "user_id": 4,
+          "name": "Trip to Japan"
+        }
+    ]
+
+    let [wallet, setWallet] = useState("Select a Wallet")
+    const handleWalletChange = (wallet) => {
+        setWallet(wallet.target.value)
+    }
+
     return (
-        <>
-        </>
+    <div>
+        {wallet}<br />
+        <select onChange={handleWalletChange}> 
+        <option value="Select a Wallet">Wallets</option>
+        {wallets.map((wallet) => <option value={wallet.name}>{wallet.name}</option>)}
+        </select>
+    </div>
     )
 }
