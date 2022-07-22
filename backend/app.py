c7558ba3e04a9aa352ccb082bb0822210ca69a38
WEN SHUFA <shufa17@gmail.com>
2022-07-22 13:49:24 +0800

    valid user will have jwt token response

--- backend/app.py
+++ backend/app.py
@@ -19,6 +19,34 @@ try:
                 user=db['mysql_user'],
                 passwd=db['mysql_password'],
                 db=db['mysql_db'])
+    
+    def token_required(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            print(request.args)
+            token = request.args.get('token') # http://127.0.0.1:500/route?token=
+            print(token)
+            if not token:
+                return jsonify({'message': 'token is missing'}), 403
+
+            try:
+                data = jwt.decode(token, app.config['SECRETY_KEY'], algorithms=["HS256"])
+                print('data: {}'.format(data))
+            except:
+                return jsonify({'message':'Token is invalid'}), 403
+            
+            return f(*args, **kwargs)
+        
+        return decorated
+
+    def getUserPassword(userName):
+        statement = "select * from user where username = '{}'".format(userName)
+        cur = connection .cursor()
+        cur.execute(statement)
+        password = cur.fetchall()
+        password = password[0][3]
+        cur.close()
+        return password
 
     class create_dict(dict): 
     
@@ -46,10 +74,22 @@ try:
     # @token_required
     def users():
         userDetails = getAllUsers()
-        print(type(userDetails))
-        print(userDetails)
         if userDetails:
             return userDetails
+        
+        
+    @app.route('/login', methods=['POST'])
+    def login():
+        auth = request.authorization
+        userInput = auth.username
+        passwordInput = auth.password
+        
+        if(userInput and getUserPassword(userInput)):
+            token = jwt.encode({'user' : auth.username, 'exp': datetime.utcnow() + timedelta(minutes=120)}, app.config['SECRETY_KEY'] )
+            return jsonify({'token': token})
+        return make_response('could not verify!', 404, {'WWW-Authenticate': 'Basic realm:"login required"'})
+    
+    
         
     if __name__ == "__main__":
         app.run()
