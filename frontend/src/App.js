f437a03850da4c66920eff79135b478152aaff49
HOWUHHOW <penghowt@gmail.com>
2022-07-22 13:50:51 +0800

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./component/Login/login";
+// import Dashboard from "./component/dashboard/dashboard";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./component/home/home";
+// import Contact from "./component/contact/contact";
+import About from "./component/about/about";
 
 function App() {
   return (
-<></>
+    <Home/> 
+    // <Login/>
+   
   );
 }
 
